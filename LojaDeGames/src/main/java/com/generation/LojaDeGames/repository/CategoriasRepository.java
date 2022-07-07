65d88b75c52dbf7aacdb6697ce5e904f721f76f3
Wellington Quadros <wellingtonquadros91@outlook.com>
2022-07-06 21:32:41 -0300

    Adicionando atividade

---
+++ LojaDeGames/src/main/java/com/generation/LojaDeGames/repository/CategoriasRepository.java
@@ -0,0 +1,14 @@
+package com.generation.LojaDeGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.LojaDeGames.model.Categorias;
+
+
+@Repository
+public interface CategoriasRepository extends JpaRepository<Categorias, Long>{
+	public List<Categorias> findAllByNomeContainingIgnoreCase (String nome);
+}
